20d469ae7c585fda85277907c1dee567a6f794ed
enfin20 <96887337+enfin20@users.noreply.github.com>
2024-01-29 21:28:42 +0100

    Update LeafletMap.svelte

--- src/lib/LeafletMap.svelte
+++ src/lib/LeafletMap.svelte
@@ -166,12 +166,18 @@
         }
       }
       leaflet.polyline(latlngs, { color: "blue" }).addTo(map);
-
+      //'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
       leaflet
-        .tileLayer("https://a.tile.opentopomap.org/{z}/{x}/{y}.png", {
-          attribution:
-            '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        })
+        .tileLayer(
+          "https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}",
+          {
+            minZoom: 0,
+            maxZoom: 20,
+            attribution:
+              '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            ext: "png",
+          }
+        )
         .addTo(map);
       var markers = [];
 
